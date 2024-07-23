95a5981ae0b9d5ce49bc6560f155935ab5139ff0
manasa8106 <auvathimanasa.123@gmail.com>
2024-07-23 17:09:00 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navbar, Container, Nav, Carousel, Card, Button, Collapse,  Row, Col, Im
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons
+
 import './App.css'; // Your custom CSS file
 import fsf from './images/fsf.jpeg';
 import nine from './images/nine.jpeg';
@@ -10,7 +11,7 @@ import sbp from './images/sbp.jpeg';
 import bigg from './images/bigg.jpeg';
 import one from './images/one.png';
 import two from './images/two.jpg';
-import three from './images/three.png';
+import three from './images/three.jpg';
 
 function App() {
 
@@ -543,7 +544,7 @@ function App() {
         <Col xs={12} md={6} lg={4}>
           <strong style={{ fontSize: '24px' }}>Phone:</strong>
           <br />
-          <span style={{ fontSize: '24px' }}>+9100632143</span>
+          <span style={{ fontSize: '24px' }}> +91 9100632143</span>
         </Col>
         <Col xs={12} md={6} lg={4}>
           <strong style={{ fontSize: '24px' }}>Gmail:</strong>
@@ -587,9 +588,9 @@ function App() {
           <li><a href="#" style={{ textDecoration: 'none', color: '#ffffff' }}>IT & Development</a></li>
           <li><a href="#" style={{ textDecoration: 'none', color: '#ffffff' }}>Cyber Security</a></li>
           <li><a href="#" style={{ textDecoration: 'none', color: '#ffffff' }}>Human Resource</a></li>
-          <li><a href="#" style={{ textDecoration: 'none', color: '#ffffff' }}>Industries</a></li>
-          <li><a href="#" style={{ textDecoration: 'none', color: '#ffffff' }}>Services</a></li>
-          <li><a href="#" style={{ textDecoration: 'none', color: '#ffffff' }}>Contact us</a></li>
+          <li><a href="#industries" style={{ textDecoration: 'none', color: '#ffffff' }}>Industries</a></li>
+          <li><a href="#services" style={{ textDecoration: 'none', color: '#ffffff' }}>Services</a></li>
+          <li><a href="#contact" style={{ textDecoration: 'none', color: '#ffffff' }}>Contact us</a></li>
         </ul>
       </Col>
       <Col xs={12} md={3} style={{ marginBottom: '20px' }}>
@@ -602,7 +603,7 @@ function App() {
           <i className="fas fa-envelope" style={{ fontSize: '36px', marginRight: '20px', color: 'white'}}></i>
           <a href="mailto:info@9thdimension.co.in" style={{ textDecoration: 'none', color: '#ffffff' }}>info@fullstackfresher.com</a><br />
           <i className="fas fa-phone" style={{ fontSize: '36px', marginRight: '100px', padding: '30px 0px' ,color: 'white'}}></i>
-          +9100632143
+          +91 9100632143
         </p>
       </Col>
       <Col xs={12} md={3} style={{ marginBottom: '20px' }}>
@@ -611,9 +612,9 @@ function App() {
           <Col xs={10} md={10} style={{ paddingLeft: '0' }}>
             <h5 style={{color: 'whitesmoke', fontSize: '30px' , fontWeight: 'bolder'}}>Follow us</h5>
             <ul style={{ listStyle: 'none', padding: '0', margin: '30px 0' }}>
-              <li><a href="#" target="_blank" style={{ textDecoration: 'none', color: '#ffffff'}}><i className="fab fa-facebook-f" style={{ fontSize: '36px', marginRight: '10px' }}></i></a></li>
-              <li><a href="#" target="_blank" style={{ textDecoration: 'none', color: '#ffffff' }}><i className="fab fa-twitter" style={{ fontSize: '36px', marginRight: '10px',marginTop: '30px' }}></i></a></li>
-              <li><a href="#" target="_blank" style={{ textDecoration: 'none', color: '#ffffff' }}><i className="fab fa-linkedin-in" style={{ fontSize: '36px', marginRight: '10px',marginTop: '30px' }}></i></a></li>
+              <li><a href="https://www.linkedin.com/company/fullstackfreshers/?originalSubdomain=in" target="_blank" style={{ textDecoration: 'none', color: '#ffffff'}}><i className="fab fa-facebook-f" style={{ fontSize: '36px', marginRight: '10px' }}></i></a></li>
+              <li><a href="https://www.youtube.com/" target="_blank" style={{ textDecoration: 'none', color: '#ffffff' }}><i className="fab fa-youtube" style={{ fontSize: '36px', marginRight: '10px',marginTop: '30px' }}></i></a></li>
+              <li><a href="https://www.linkedin.com/company/fullstackfreshers/?originalSubdomain=in" target="_blank" style={{ textDecoration: 'none', color: '#ffffff' }}><i className="fab fa-linkedin-in" style={{ fontSize: '36px', marginRight: '10px',marginTop: '30px' }}></i></a></li>
             </ul>
           </Col>
         </Row>
